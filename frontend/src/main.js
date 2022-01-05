d7c43edbcffe7e7634e8b846ba8b7c9cf6c2690f
Matheus <matheussilveirasergio@gmail.com>
2022-01-05 17:55:59 -0300

    realizado telas de cadstro de categoria e cadastro de usuario atravez da tela admin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from './config/router'
 
@@ -11,7 +12,7 @@ Vue.config.productionTip = false
 
 //TEMPORARIO
 
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik1hdGhldXMiLCJlbWFpbCI6Im1hdGhldXNAdGVzdGUuY29tLmJyIiwiYWRtaW4iOnRydWUsImlhdCI6MTY0MDkwMDM2MywiZXhwIjoxNjQxMTU5NTYzfQ.AqsZwmqE8FTZWGAwHJr7TbHheskeuCGE1DeDFfulEVA'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik1hdGhldXMiLCJlbWFpbCI6Im1hdGhldXNAdGVzdGUuY29tLmJyIiwiYWRtaW4iOnRydWUsImlhdCI6MTY0MTMxOTU4MywiZXhwIjoxNjQxNTc4NzgzfQ.UqEHUriK9Yfw9Mh6Mg8ccEFHCPKVU4BLHOqwj2spC6A'
 
 new Vue({
   store,
